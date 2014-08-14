25dc50d4fa688e606237730861a86223347311a6
Dal Rupnik <legoless@gmail.com>
2014-08-14 14:16:23 +0200

    Prepare text fix

--- scripts/deploy/prepare.sh
+++ scripts/deploy/prepare.sh
@@ -261,9 +261,9 @@ echo '[PREPARE]: Downloading new provisioning profile...'
 #$($CUPERTINO_PATH profiles:download $PROFILE_NAME --team $DEVELOPER_TEAM --username $DEVELOPER_USERNAME --password $DEVELOPER_PASSWORD)
 DOWNLOAD=$($CUPERTINO_PATH profiles:download $PROFILE_NAME --team $DEVELOPER_TEAM --username $DEVELOPER_USERNAME --password $DEVELOPER_PASSWORD --trace)
 
-echo '[PREPARE]: Provisioning profile download completed.'
-
 echo '[PREPARE]: '$DOWNLOAD
+
+echo '[PREPARE]: Download completed (provisioning profile).'
 
 PROFILE_NAME=`find . -type f -name "*.mobileprovision" | head -n1`
 
