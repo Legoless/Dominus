6885ce3142fc09cab54c2bf360b5d53ed60d4676
Dal Rupnik <legoless@gmail.com>
2014-10-06 15:26:46 +0200

    Added init action for tests on CI

--- dominus.sh
+++ dominus.sh
@@ -129,7 +129,7 @@ integrate()
   # from point 0
   #
 
-  if [ "$CI" = true ] && [ "$ACTION" != "run_tests" ] && [ "$ACTION" != "report" ]; then
+  if [ "$CI" = true ] && [ "$ACTION" != "report" ]; then
     init
     #library
   fi
