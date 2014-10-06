cbee1d17693ecf9a2b2734c0a1e91ee31f7aa672
Dal Rupnik <legoless@gmail.com>
2014-10-06 15:49:15 +0200

    Added plain command for testing

--- scripts/library/tests.sh
+++ scripts/library/tests.sh
@@ -163,7 +163,8 @@ execute_test()
 
       #eval $TEST_COMMAND' -reporter junit:./report/'$LOG_REPORT_PATH'.xml' > './report/'$LOG_REPORT_PATH'_xcode.log'
       eval $TEST_COMMAND' -reporter plain:./report/'$LOG_REPORT_PATH'.xml'
-
+      eval $TEST_COMMAND
+      
       exit 1
     fi
   fi
